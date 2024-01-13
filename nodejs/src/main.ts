cede32e2a4948fefb1615d46224d8e5a9b7a6996
Vinicius Sakai <vinicius.sakai@tuta.io>
2024-01-12 23:08:25 -0300

    fix: env port variable error

--- nodejs/src/main.ts
+++ nodejs/src/main.ts
@@ -4,14 +4,14 @@ import { PostgresPool } from "./database/db";
 import { routes } from "./routes";
 
 const server = fastify();
-const PORT = 3000 || process.env.PORT;
+const PORT = Number(process.env.APP_PORT) || 3000;
 const database = PostgresPool.geInstance();
 
 database.init();
 
 server.register(routes, {});
 
-server.listen({ port: PORT }, (err, address) => {
+server.listen({ port: PORT, host: "0.0.0.0" }, (err, address) => {
   if (err) {
     console.log(err);
     process.exit(1);
